cd7edb4a960c2941f1f1ccc4607e4751bb853677
Kuldeep Singh <65607120+Kuldeepdawar@users.noreply.github.com>
2021-02-15 09:38:07 +1100

    update

---
+++ src/components/Showcase.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Showcase = () => {
+    return (
+        <div className="show">
+            <div className="container rel">
+           <div className="show__content">
+               <h1>Upload Your Resume</h1>
+               <h1>Get Job Matches.</h1>
+               </div>
+
+               <div className="show__card">
+                   <h6 className="show__card-h6">OUR HAPPY CLIENTS</h6>
+                   <div className="show__card-h4">What Client's Say?</div>
+                   <div className="show__card-text">I have known Jess and Mathews over the last 1 years and always a pleasure speaking with her over the phone. My experience has always been great especially with my Internship/Resume Updated recently. I'm totally impressed from the first time I saw it online. Highly recommend her knowledge and expertise..</div>
+               </div>
+               </div> 
+        </div>
+    )
+}
+
+export default Showcase
